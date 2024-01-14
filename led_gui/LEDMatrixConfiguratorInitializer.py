ef9c01a428b09f857ee45b950b2648d5404d3e29
DorienComp <dpenebac@purdue.edu>
2024-01-13 17:04:11 -1000

    added display vs gui configs

--- led_gui/LEDMatrixConfiguratorInitializer.py
+++ led_gui/LEDMatrixConfiguratorInitializer.py
@@ -25,36 +25,60 @@ class LEDMatrixConfiguratorInitializer:
     self.root.resizable(False, False)
 
     # Labels
-    self.column_size_config_label = tk.Label(root, text="Column Size Config")
-    self.column_size_config_label.grid(row=0, column=0)
+    self.gui_column_size_config_label = tk.Label(root, text="Column Size Config")
+    self.gui_column_size_config_label.grid(row=0, column=0)
 
-    self.row_size_config_label = tk.Label(root, text="Row Size Config")
-    self.row_size_config_label.grid(row=1, column=0)
+    self.gui_row_size_config_label = tk.Label(root, text="Row Size Config")
+    self.gui_row_size_config_label.grid(row=1, column=0)
+
+    self.matrix_column_size_config_label = tk.Label(root, text="Column Size Config")
+    self.matrix_column_size_config_label.grid(row=2, column=0)
+
+    self.matrix_row_size_config_label = tk.Label(root, text="Row Size Config")
+    self.matrix_row_size_config_label.grid(row=3, column=0)
 
     # Default column and row sizes
-    DEFAULT_COLUMN_SIZE = "64"
-    DEFAULT_ROW_SIZE = "64"
+    DEFAULT_GUI_COLUMN_SIZE = "32"
+    DEFAULT_GUI_ROW_SIZE = "32"
+    DEFAULT_DISPLAY_COLUMN_SIZE = "64"
+    DEFAULT_DISPLAY_ROW_SIZE = "64"
 
-    # Size Configuration
-    self.column_size_config = tk.StringVar()
-    self.column_size_config_entry = tk.Entry(root, width=10, textvariable=self.column_size_config)
-    self.column_size_config_entry.insert(0, DEFAULT_COLUMN_SIZE)
-    self.column_size_config_entry.grid(row=0, column=1)
+    # GUI Size Configuration
+    self.gui_column_size_config = tk.StringVar()
+    self.gui_column_size_config_entry = tk.Entry(root, width=10, textvariable=self.gui_column_size_config)
+    self.gui_column_size_config_entry.insert(0, DEFAULT_GUI_COLUMN_SIZE)
+    self.gui_column_size_config_entry.grid(row=0, column=1)
 
-    self.row_size_config = tk.StringVar()
-    self.row_size_config_entry = tk.Entry(root, width=10, textvariable=self.row_size_config)
-    self.row_size_config_entry.insert(0, DEFAULT_ROW_SIZE)
-    self.row_size_config_entry.grid(row=1, column=1)
+    self.gui_row_size_config = tk.StringVar()
+    self.gui_row_size_config_entry = tk.Entry(root, width=10, textvariable=self.gui_row_size_config)
+    self.gui_row_size_config_entry.insert(0, DEFAULT_GUI_ROW_SIZE)
+    self.gui_row_size_config_entry.grid(row=1, column=1)
+
+    # Display Size Configuration
+    self.matrix_column_size_config = tk.StringVar()
+    self.matrix_column_size_config_entry = tk.Entry(root, width=10, textvariable=self.matrix_column_size_config)
+    self.matrix_column_size_config_entry.insert(0, DEFAULT_DISPLAY_COLUMN_SIZE)
+    self.matrix_column_size_config_entry.grid(row=2, column=1)
+
+    self.matrix_row_size_config = tk.StringVar()
+    self.matrix_row_size_config_entry = tk.Entry(root, width=10, textvariable=self.matrix_row_size_config)
+    self.matrix_row_size_config_entry.insert(0, DEFAULT_DISPLAY_ROW_SIZE)
+    self.matrix_row_size_config_entry.grid(row=3, column=1)
 
     # Live/Design Configuration
     self.selected_option = tk.StringVar()
     self.selected_option.set("Live")
     self.mode_menu = tk.OptionMenu(root, self.selected_option, "Live", "Design", command=lambda x: self.switch(self.selected_option.get()))
-    self.mode_menu.grid(row=2, column=0)
+    self.mode_menu.grid(row=4, column=0)
 
     # Submit Button
     self.submit_button = tk.Button(root, text="Submit", command=root.destroy) # root.destroy closes this tk window
-    self.submit_button.grid(row=3, column=0)
+    self.submit_button.grid(row=5, column=0)
   
   def switch(self, option):
     self.selected_option.set(option)
+
+if __name__ == "__main__":
+  root = tk.Tk()
+  app = LEDMatrixConfiguratorInitializer(root)
+  root.mainloop()
