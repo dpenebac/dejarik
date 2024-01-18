159653f46d91897aacea874fcac6ce15abfdad7f
DorienComp <dpenebac@purdue.edu>
2024-01-17 14:08:30 -1000

    fixed json formatting

--- scripts/load_dejarik.py
+++ scripts/load_dejarik.py
@@ -2,33 +2,35 @@ import numpy as np
 from PIL import Image, ImageDraw
 import json
 
-def hex_to_rgb(hex_color):
-    return tuple(int(hex_color[i:i+2], 16) for i in (1, 3, 5))
-
 def load_image(json_file_path):
     # Load JSON data from file
     with open(json_file_path, "r") as json_file:
         data = json.load(json_file)
 
     # Extract matrix size and colors from the loaded data
-    matrix_size = data["matrix_size"]
-    pixel_colors_list = data["matrix_colors"]
-
-    # Convert hex color codes to RGB tuples
-    pixel_colors = np.array([[hex_to_rgb(color) for color in row] for row in pixel_colors_list], dtype=np.uint8)
+    matrix_columns = data["matrix_columns"]
+    matrix_rows = data["matrix_rows"]
 
     # Create a blank image
-    image = Image.new('RGB', (matrix_size, matrix_size))
+    image = Image.new('RGB', (matrix_columns, matrix_rows))
+
+    # Extract matrix size and colors from the loaded data
+    pixel_colors_list = data["matrix_layout"]
+    # Convert hex color codes to RGB tuples
+    pixel_colors = np.array([[hex_to_rgb(color) for color in row] for row in pixel_colors_list])
 
     # Draw on the image using the loaded colors
     draw = ImageDraw.Draw(image)
-    for i in range(matrix_size):
-        for j in range(matrix_size):
+    for i in range(matrix_rows):
+        for j in range(matrix_columns):
             # Use the * operator to unpack the RGB tuple
             draw.point((j, i), fill=tuple(pixel_colors[i, j]))
 
     # Save the reconstructed image
     image.save("reconstructed_dejarik_board.png")
+
+def hex_to_rgb(hex_color):
+    return tuple(int(hex_color[i:i+2], 16) for i in (1, 3, 5))
 
 if __name__ == "__main__":
     load_image("dejarik_board.json")
